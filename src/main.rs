246efbb5116ff6d6ab607dbc6fb4b8123e136c55
Axel Polin <president@scenab.re>
2023-03-13 19:10:16 +0100

    Simple midi mesg handler

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rainout::{MidiControlScheme, ProcessInfo, ProcessHandler, StreamInfo, Backend, RainoutConfig, RunOptions, AutoOption, MidiPortConfig, MidiConfig, AudioDeviceConfig};
+use rainout::{MidiControlScheme, ProcessInfo, ProcessHandler, StreamInfo, Backend, RainoutConfig, RunOptions, AutoOption, MidiPortConfig, MidiConfig, RawMidi, AudioDeviceConfig};
 use std::string::String;
 use simple_logger::SimpleLogger;
 
@@ -18,7 +18,8 @@ fn main() {
     };
 
     let midi_device_id = rainout::DeviceID {
-        name: String::from("a2j:Midi Through [14] (capture): [0] Midi Through Port-0"),
+        name: String::from("jack-keyboard:midi_out"),
+        // name: String::from("a2j:Midi Through [14] (capture): [0] Midi Through Port-0"),
         identifier: None,
     };
 
@@ -72,7 +73,7 @@ fn main() {
 
     let stream_handle = rainout::run(&config,&run_opt,process).unwrap(); 
 
-    std::thread::sleep(std::time::Duration::from_secs(20));
+    std::thread::sleep(std::time::Duration::from_secs(200));
 
     let _ = stream_handle;
 }
@@ -81,6 +82,44 @@ fn main() {
 pub struct MidiProcessor {
     mesg: String,
     test: bool,
+}
+
+// pub struct MidiMesg {
+//     cmd: u8,
+//     param1: u8,
+//     param2: Option<u8>,
+// }
+
+// pub enum MidiMesg {
+//     Cmd,
+//     Param1,
+//     Param2,
+// }
+
+pub fn process_midi_mesg(event: &[u8]) -> u8 {
+
+// Command  Meaning      # parameters  param 1      param 2
+// 0x80      Note-off    2              key          velocity
+// 0x90      Note-on     2              key          velocity
+// 0xA0      Aftertouch  2              key          touch
+// 0xB0      Cont CTRL   2              ctrl #       ctrl value
+// 0xC0      Patch chg   2              instr # 	
+// 0xD0      Chan Press  1              pressure     X
+// 0xE0      Pitch bend  2              lsb (7 bits) msb (7 bits)
+// 0xF0      (non-musical commands) 			
+    
+    let cmd: u8 = event[0];
+
+    match cmd {
+        0xA0|0xC0|0xE0 => println!("Command not used in Blender Midi"),
+        0x80 => println!("Note off : {:?} (vel : {:?})", event[1], event[2]),
+        0x90 => println!("Note on : {:?} (vel : {:?})", event[1], event[2]),
+        0xD0 => println!("Channel press : {:?}", event[1]),
+        0xFF => panic!("User press PANIC on keyboard, shutdown client !"),
+        _ => println!("Unkown event : {:04X?}", event),
+    }
+
+    return 0;
 }
 
 impl ProcessHandler for MidiProcessor {
@@ -98,8 +137,20 @@ impl ProcessHandler for MidiProcessor {
 
     fn process<'a>(&mut self, proc_info: ProcessInfo<'a>) {
 
-        // println!("{:?}",proc_info.midi_inputs[0].events());
+        let events = proc_info.midi_inputs[0].events();
 
+        if events.len() != 0 {
+            if events.len() > 1 {
+                for event in events.iter() {
+                    process_midi_mesg(event.data());
+                }
+            } else {
+                let event: RawMidi = events[0];
+                process_midi_mesg(event.data());
+            };
+        };
     }
 
 }
+
+
