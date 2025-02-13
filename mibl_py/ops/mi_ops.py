ed3a8a6169d227f1b45fecbf6a9e696c1b1d27dc
lynerlok <univ_apolin@protonmail.com>
2025-02-13 16:56:07 +0100

    Fix timers errors, wrap the two ops in one update_server_state

--- mibl_py/ops/mi_ops.py
+++ mibl_py/ops/mi_ops.py
@@ -1,11 +1,16 @@
 # from mibllib import mi_start_server, mi_stop_server, mi_get_midi_mesg, mi_set_midi_mesg
 from bpy.types import Operator
 from bpy.app import timers
-from bpy import context as C
+import functools
+import random
+
+update_func = None
 
 
 def mi_get_midi_mesg():
     print("Call to mi_get_midi_mesg")
+    ran_num = random.randrange(0, 255, 1)
+    return (ran_num, ran_num, ran_num)
 
 
 def mi_start_server():
@@ -16,41 +21,41 @@ def mi_stop_server():
     print("Call to mi_stop_server")
 
 
-def update_midi_value():
-    C.scene.mibl.mi_input_mesg = mi_get_midi_mesg()
-    return 1.0  # Update Interval 1s
+def update_midi_value(context):
+    scene = context.scene
+    scene.mibl.mi_input_mesg = mi_get_midi_mesg()
+    return 1.0  # update interval 1s
 
 
-def start_server(scene):
-    if not scene.mibl.mi_run_server:
-        mi_start_server()
-        scene.mibl.midi_server_running = True
-        timers.register(update_midi_value)
-        print("MIDI Server Started")
-
-
-def stop_server(scene):
-    if scene.mibl.mi_run_server:
-        mi_stop_server()
-        scene.mibl.midi_server_running = False
-        scene.mibl.midi_server_value = ""
-        timers.unregister(update_midi_value)
-        print("MIDI Server Stopped")
-
-
-class MI_BL_OT_start_server(Operator):
+class MI_BL_OT_update_server_state(Operator):
     bl_label = "Start MIDI Server"
-    bl_idname = "mibl.start_server"
+    bl_idname = "mibl.set_server_state"
 
     def execute(self, context):
-        start_server(C.scene)
-        return {'FINISHED'}
+        global update_func
 
+        scene = context.scene
+        if not scene.mibl.mi_run_server:
+            update_func = functools.partial(update_midi_value, context)
+            mi_start_server()
+            scene.mibl.mi_run_server = True
+            timers.register(
+                update_func,
+                first_interval=2.0
+            )
+            self.report({'INFO'}, 'MIDI Server Started')
+            print("MIDI Server Started")
+        else:
+            mi_stop_server()
+            scene.mibl.mi_run_server = False
+            scene.mibl.mi_input_mesg = (0, 0, 0)
+            scene.mibl.mi_output_mesg = (0, 0, 0)
+            if timers.is_registered(update_func):
+                timers.unregister(update_func)
+            else:
+                print("Something is strange, function seems not registered…")
 
-class MI_BL_OT_stop_server(Operator):
-    bl_label = "Stop MIDI Server"
-    bl_idname = "mibl.stop_server"
+            self.report({'INFO'}, 'MIDI Server Stopped')
+            print("MIDI Server Stopped")
 
-    def execute(self, context):
-        stop_server(C.scene)
         return {'FINISHED'}
