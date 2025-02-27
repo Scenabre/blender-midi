75b2ad0d5a0f3831a85e1195b693d1cecbb8ea14
Axel Polin <axel.polin@scenabre.art>
2025-02-27 19:10:51 +0100

    Truncate digit vector in math utils if number of digits exceed the vector size input

--- mibl_rs/src/midi_server/math_utils.rs
+++ mibl_rs/src/midi_server/math_utils.rs
@@ -1,3 +1,5 @@
+use rand::seq::IndexedRandom;
+
 pub fn split_digits(number_to_split: &usize, vector_size: u8) -> Vec<u8> {
     let mut digits = Vec::<u8>::with_capacity(vector_size.into());
     let mut number = *number_to_split;
@@ -9,6 +11,8 @@ pub fn split_digits(number_to_split: &usize, vector_size: u8) -> Vec<u8> {
     while digits.len() < vector_size.into() {
         digits.push(0);
     }
+
+    digits.truncate(vector_size.into());
 
     digits
 }
