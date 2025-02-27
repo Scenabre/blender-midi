052d9b84b46b411b285aa830c4a576bea98a9ec2
Axel Polin <axel.polin@scenabre.art>
2025-02-27 10:38:17 +0100

    Add a split digit math util

---
+++ mibl_rs/src/midi_server/math_utils.rs
@@ -0,0 +1,14 @@
+pub fn split_digits(number_to_split: &usize, vector_size: u8) -> Vec<u8> {
+    let mut digits = Vec::<u8>::with_capacity(vector_size.into());
+    let mut number = *number_to_split;
+    while number != 0 {
+        digits.push((number % 10).try_into().unwrap());
+        number /= 10;
+    }
+
+    while digits.len() <= vector_size.into() {
+        digits.push(0);
+    }
+
+    digits
+}
