f2f468ec2cfb90f24f94b07b6293c0cd166974ff
Axel Polin <axel.polin@scenabre.art>
2025-02-27 10:56:28 +0100

    Add forgotten MC instruction : timestamp, pan LED. Some experiments, it works but needs fixing

--- mibl_rs/src/midi_server/midi_send_mesg.rs
+++ mibl_rs/src/midi_server/midi_send_mesg.rs
@@ -1,4 +1,5 @@
 use crate::midi_server::container::{RawMidi, MAX_MIDI_MSG_SIZE};
+use crate::midi_server::math_utils::split_digits;
 //use crate::midi_process_mesg::MidiMesg;
 //
 // CHANNEL VOICE MESG
@@ -39,19 +40,6 @@ pub fn make_raw_midi_mesg(stamp: &u64, mesg: &Vec<u8>) -> Result<RawMidi, String
     // Con CTRL = CC
     // Chan press = CP
     // Pitch bend = PB
-
-    // Trim if needed
-
-    //let mut last_non_zero = raw_midi.data().len();
-    //while last_non_zero > 0 && raw_midi.data()[last_non_zero - 1] == 0 {
-    //    last_non_zero -= 1;
-    //}
-    //
-    //// Ensure the trimmed slice has at least 3 elements
-    //let final_len = if last_non_zero >= 3 { last_non_zero } else { 3 };
-    //
-    //// Create a new slice with the final length
-    //let result = &raw_midi.data()[..final_len];
 
     let raw_midi_mesg: Result<RawMidi, String> = match RawMidi::new(*stamp, mesg) {
         Ok(raw_midi) => Ok(raw_midi),
@@ -122,6 +110,49 @@ pub fn send_midi_mesg(mesg: &[RawMidi]) -> bool {
     false
 }
 
+pub fn timestamp_gen(
+    hours: usize,
+    minutes: usize,
+    seconds: usize,
+    frames: usize,
+) -> Result<Vec<RawMidi>, String> {
+    let mut raw_midi_timestamp: Vec<RawMidi> = Vec::new();
+    let stamp: u64 = 0;
+    // frames : 000 -> 3E7
+    // sec 00 -> 3C
+    // min 00 -> 3C
+    // hours 000 -> 3EC
+
+    let frames = split_digits(&frames, 3);
+    let seconds = split_digits(&seconds, 2);
+    let minutes = split_digits(&minutes, 2);
+    let hours = split_digits(&hours, 3);
+
+    let mut timestamp: Vec<u8> = vec![];
+    timestamp.extend(frames);
+    timestamp.extend(seconds);
+    timestamp.extend(minutes);
+    timestamp.extend(hours);
+
+    for (idx, value) in timestamp.iter().enumerate() {
+        let digit = idx + 0x40;
+        match make_raw_midi_mesg(&stamp, &vec![0xB0, digit as u8, (*value + 0x30)]) {
+            Ok(raw_midi) => raw_midi_timestamp.push(raw_midi),
+            Err(err) => println!("Unable to create message for timestamp ! {}", err),
+        }
+    }
+
+    Ok(raw_midi_timestamp)
+}
+
+pub fn assign_gen(assign: u8) -> Result<RawMidi, String> {
+    todo!()
+}
+
+pub fn reset_mc_device() -> Result<Vec<RawMidi>, String> {
+    todo!()
+}
+
 pub fn initialize_mc_device() -> Result<Vec<RawMidi>, String> {
     log::info!("Initilizing midi device : ");
     let mut raw_midi_mesg: Vec<RawMidi> = Vec::new();
@@ -136,8 +167,9 @@ pub fn initialize_mc_device() -> Result<Vec<RawMidi>, String> {
     //raw_midi_mesg.push(make_raw_midi_mesg_fast(time,sysx_mesg).unwrap());
     //raw_midi_mesg.push(make_raw_midi_mesg_fast(time,sysx_mesg_2).unwrap());
 
+    let mut midi_mesg: Vec<u8> = Vec::with_capacity(MAX_MIDI_MSG_SIZE);
+
     for pb_idx in 0..9 {
-        let mut midi_mesg: Vec<u8> = Vec::with_capacity(MAX_MIDI_MSG_SIZE);
         let pb_num = pitch_bend_prefix + pb_idx;
 
         let (lsb, msb) = convert_value_to_lsb_msb(0.75);
@@ -160,6 +192,34 @@ pub fn initialize_mc_device() -> Result<Vec<RawMidi>, String> {
         midi_mesg.clear();
 
         //time += 100;
+    }
+
+    for pan_idx in 0..9 {
+        midi_mesg.push(0xB0);
+        midi_mesg.push(0x30 + pan_idx);
+        midi_mesg.push(0x0D);
+
+        match make_raw_midi_mesg_fast(&time, &midi_mesg) {
+            Ok(raw_midi) => {
+                log::info!("Initializing pan LED #{}", pan_idx);
+                raw_midi_mesg.push(raw_midi);
+            }
+            Err(..) => {
+                log::error!("Unable to trigger event abort");
+                //Err("Unable to initialize mc device !".to_string())
+            }
+        };
+
+        midi_mesg.clear();
+    }
+
+    match timestamp_gen(1, 45, 30, 250) {
+        Ok(timestamp) => {
+            for midi_mesg in timestamp {
+                raw_midi_mesg.push(midi_mesg);
+            }
+        }
+        Err(err) => println!("Unable to generate timestamp : {}", err),
     }
 
     log::info!("Initilizing LCD display #1");
