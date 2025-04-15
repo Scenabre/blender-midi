bffbf3412a91c4d3bdf8414785e708818529399c
Axel Polin <axel.polin@scenabre.art>
2025-04-15 15:50:10 +0200

    Add props : vecout, recipe, ingredient, lcdparams, vpotparams, trigger, faders, user_btn, timestamp, sysparams. Cleanup propsmibl

--- mibl_py/props/mi_props.py
+++ mibl_py/props/mi_props.py
@@ -1,5 +1,113 @@
 from bpy.types import PropertyGroup
-from bpy.props import BoolProperty, IntVectorProperty, IntProperty
+from bpy.props import BoolProperty, IntVectorProperty, IntProperty, CollectionProperty, StringProperty, FloatProperty, PointerProperty
+
+MIDI_IN_SIZE = 8
+MIDI_OUT_SIZE = 32
+
+
+def clean_coll(coll):
+    for idx, vec in enumerate(coll):
+        coll.remove(idx)
+
+
+class MI_BL_VecOut(PropertyGroup):
+    vec_out: IntVectorProperty(size=MIDI_OUT_SIZE)
+
+
+class MI_BL_Ingredient(PropertyGroup):
+    ing_name: StringProperty()
+    midi_in: IntVectorProperty(size=MIDI_IN_SIZE)
+    midi_out: CollectionProperty(
+        type=MI_BL_VecOut
+    )
+    opt_val: FloatProperty()
+
+    def clean_midi_out(self):
+        for idx, vec in enumerate(self.midi_out):
+            self.midi_out.remove(idx)
+
+
+class MI_BL_Recipe(PropertyGroup):
+    ingredients: CollectionProperty(
+        type=MI_BL_Ingredient
+    )
+
+    def clean_ingredients(self):
+        for idx, vec in enumerate(self.ingredients):
+            self.ingredients.remove(idx)
+
+
+class MI_BL_TriggerProp(PropertyGroup):
+    triggerIdx: IntProperty()
+    triggerValue: FloatProperty()
+
+
+class MI_BL_LcdParams(PropertyGroup):
+    lcd_num: IntProperty()
+    line_num: IntProperty()
+    string: StringProperty()
+
+
+class MI_BL_VPotParams(PropertyGroup):
+    vpot_idx: IntProperty()
+    vpot_mode: IntProperty()
+    vpot_val: IntProperty()
+
+
+class MI_BL_FaderParams(PropertyGroup):
+    fader_num: IntProperty()
+    fader_value: FloatProperty()
+
+
+class MI_BL_ChanBtnParams(PropertyGroup):
+    chan_num: IntProperty()
+    btn_num: IntProperty()
+    btn_status: BoolProperty()
+
+
+class MI_BL_TimestampParams(PropertyGroup):
+    hours: IntProperty()
+    minutes: IntProperty()
+    seconds: IntProperty()
+    frames: IntProperty()
+
+
+class MI_BL_SysParams(PropertyGroup):
+    timestamp: PointerProperty(
+        type=MI_BL_TimestampParams
+    )
+    lcd_vec: CollectionProperty(
+        type=MI_BL_LcdParams
+    )
+    lcd_mesg: StringProperty()
+    vpot_vec: CollectionProperty(
+        type=MI_BL_VPotParams
+    )
+    faders: CollectionProperty(
+        type=MI_BL_FaderParams
+    )
+    user_btns: CollectionProperty(
+        type=MI_BL_ChanBtnParams
+    )
+    fps: IntProperty()
+
+    def clean_lcd_vec(self):
+        clean_coll(self.lcd_vec)
+
+    def clean_vpot_vec(self):
+        clean_coll(self.vpot_vec)
+
+    def clean_faders(self):
+        clean_coll(self.faders)
+
+    def clean_user_btns(self):
+        clean_coll(self.user_btns)
+
+    def clean_all(self):
+        self.clean_lcd_vec()
+        self.clean_vpot_vec()
+        self.clean_faders()
+        self.clean_user_btns()
 
 
 class PropsMiBl(PropertyGroup):
@@ -29,13 +137,15 @@ class PropsMiBl(PropertyGroup):
         description="Toggle Mackie Device system control",
         default=True
     )
-    mi_midi_channel: IntVectorProperty(
-        name="mi_midi_channel",
-        description="Wait data from this channel",
-        min=0,
-        max=16,
-        soft_min=1,
-        soft_max=16,
-        size=16,
-        default=tuple([0] * 16)
+    mi_trigger_list: CollectionProperty(
+        name="mi_trigger_list",
+        description="Trigger from recipe",
+        type=MI_BL_TriggerProp,
+    )
+    mi_recipe: CollectionProperty(
+        name="mi_recipe",
+        type=MI_BL_Ingredient
+    )
+    mi_init: PointerProperty(
+        type=MI_BL_SysParams
     )
