36489e1d7b2dea56ed12c794020b2adc51470369
Axel Polin <axel.polin@scenabre.art>
2025-04-22 18:26:43 +0200

    Cleanup unused props and add recipe props (recipe container, footprint)

--- mibl_py/props/mi_props.py
+++ mibl_py/props/mi_props.py
@@ -111,22 +111,6 @@ class MI_BL_SysParams(PropertyGroup):
 
 
 class PropsMiBl(PropertyGroup):
-    mi_output_mesg: IntVectorProperty(
-        name="mi_output_mesg",
-        description="MIDI Interactive output message",
-        default=(0, 0, 0),
-        size=3,
-        min=0,
-        max=255
-    )
-    mi_input_mesg: IntVectorProperty(
-        name="mi_input_mesg",
-        description="MIDI Interactive output message",
-        default=(0, 0, 0),
-        size=3,
-        min=0,
-        max=255
-    )
     mi_run_server: BoolProperty(
         name="mi_run_server",
         description="MIDI Interactive control",
@@ -148,4 +132,12 @@ class PropsMiBl(PropertyGroup):
     )
     mi_init: PointerProperty(
         type=MI_BL_SysParams
+    )
+    mi_recipe_need_update: BoolProperty(
+        name="mi_recipe_need_update",
+        description="Send recipe update signal to server"
+    )
+    mi_recipe_footprint: StringProperty(
+        name="mi_recipe_footprint",
+        description="The hash value of the recipe"
     )
