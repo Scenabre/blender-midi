d85a75b1014fcfd7e9ebfec21a58deaab5a0974e
Axel Polin <axel.polin@scenabre.art>
2025-04-22 18:20:38 +0200

    Relocate the timestamp fn generator

--- mibl_py/utils/mibl_utils.py
+++ mibl_py/utils/mibl_utils.py
@@ -14,3 +14,27 @@ def fill_array(array, size):
         for idx in range(arr_len, size):
             to_fill.append(int(-1))
     return to_fill
+
+
+def gen_timestamp(fps, curr_frame, mode):
+
+    hours = 0
+    minutes = 0
+    seconds = 0
+    frames = 0
+
+    match mode:
+        case 0:
+            hours = int(curr_frame / (3600*fps))
+            minutes = int(curr_frame / (60*fps) % 60)
+            seconds = int(curr_frame / fps % 60)
+            frames = curr_frame % fps
+        case 1:
+            frames = curr_frame % 1000
+            seconds = int(curr_frame / 1000) % 100
+            minutes = int(curr_frame / 100000) % 100
+            hours = int(curr_frame / 10000000) % 1000
+        case _:
+            print("[WARNING MIBL] Timestamp gen mode unknown (0 or 1): ", mode)
+
+    return [hours, minutes, seconds, frames]
