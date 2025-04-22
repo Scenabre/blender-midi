1fbf98f5d4ae7bf53be1bf25121e76dceb52d319
Axel Polin <axel.polin@scenabre.art>
2025-04-22 18:17:26 +0200

    Just remove a debug mesg from mi_update

--- mibl_py/node_tree/mi_update.py
+++ mibl_py/node_tree/mi_update.py
@@ -4,5 +4,4 @@ import bpy
 def execute_active_node_tree():
     for node_tree in bpy.data.node_groups:
         if hasattr(node_tree, '_isMiNodetree'):
-            print("Trigger node tree execute")
             node_tree.execute(bpy.context)
